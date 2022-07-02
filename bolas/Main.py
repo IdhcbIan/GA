1b0de549730865a43e0b2234716d36fd04d597b8
ian <ian@pop-os.localdomain>
2022-07-02 12:16:17 -0300

    bolas

--- bolas/Main.py
+++ bolas/Main.py
@@ -4,19 +4,53 @@ import pygame
 
 pygame.init()
 
-win = pygame.display.set_mode((500, 500))
+win = pygame.display.set_mode((1000, 500))
 clock = pygame.time.Clock()
-
+ 
 pygame.display.set_caption("Bolas")
+
+class bola():
+    def __init__(self, x, h, r, vel1, cor, hb):
+        self.x = int(x)
+        self.h = int(h)
+        self.r = int(r)
+        self.vel1 = int(vel1)
+        self.cor = cor
+        self.hb = int(hb)
+        
+
+
+
+    def draw(self, x, h, r, cor):
+        pygame.draw.circle(win, cor, (x, h), r)
+
 
 x1 = 50
 x2 = 200
-y = 100
+h = 100
 widht = 40
-height = 60
-vel = 5
-move = False
+height = 60             # 3 = 1m/s
+inp  =  int(input("qual a velocidade? "))  
+vel1 = inp
+vel2 = 1
 
+ine = False
+ela = False
+
+r = 20
+cor = (0, 0, 0)
+hb1 = x1 + r
+hb2 = x2 - r
+
+col = False
+
+
+#  classes 
+
+
+
+
+#-------------------------------------------------------------------------------------
 run = True
 
 while run:
@@ -25,29 +59,73 @@ while run:
         if event.type == pygame.QUIT:
             run = False
     
+   
+
     keys = pygame.key.get_pressed()
 
 
-    if keys[pygame.K_SPACE]:
-        move = True
 
-    if move == True:    
-        x1 += vel
+    if keys[pygame.K_q]:
+        ine = True
 
-        if x1 + 35 == x2 and x2 + 20 != 500:
-            if x1 + 35 == x2:
-                x2 += vel
-            else: 
-                vel = -10
+    if keys[pygame.K_e]:
+        ela = True
+        print("aqui")
+        
 
-        win.fill((0, 0, 0))
-        pygame.draw.circle(win, (255, 0, 0), (x1, y), 20)
-        pygame.draw.circle(win, (0, 0, 255), (x2, y), 20)
-        pygame.display.update()
+    win.fill((0, 0, 0))
+
+    hb1 = x1 + r
+    hb2 = x2 - r
+
+
+    #Elastico -----------------------
+    if ela == True:
+        if hb1 > hb2:
+            col = True
+        else:
+            pass
+        
+        if col == True:
+            vel1 == 0
+            vel2 = inp
+            print(vel2)
+            x2 += vel2
+        else:
+            x1 += vel1
+        
+        
+    #---------------------------------
+   
+    #inelastico -----------------------
+    if ine == True:
+        if hb1 > hb2:
+            
+            vel1 == vel1
+            x1 += vel1/2
+            x2 = x1 + (2*r - 1) 
+            print(vel1)
+
+        else:
+            
+            x1 += vel1 
+    #---------------------------------
+
+
+    luke = bola(x1 ,h ,20 , vel1, (255, 0, 0), hb1)
+    boulos = bola(x2, h ,20 ,vel2, (0, 0, 255), hb2)     
+    #print(hb1) 
+    #print(hb2)
+
+  
+    luke.draw(x1, h, 20, (255, 0, 0))
+    boulos.draw(x2, h, 20, (0, 0, 255))
+     
+    pygame.display.update()
+    #---------------------------------------------------------------------------------------------
+else:
     
-    else:
-    
         pygame.display.update()
 
 
-pygame.quit() 
+#pygame.quit() 
